8308b8500025472fc4a0131a3646d3b4579dca10
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2019-11-02 17:00:57 +0100

    Accept more listeners

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [![Coverage Status](https://coveralls.io/repos/github/szmarczak/defer-to-connect/badge.svg?branch=master)](https://coveralls.io/github/szmarczak/defer-to-connect?branch=master)
 
-Once you receive the socket, it may be already connected.<br>
+Once you receive the socket, it may be already connected (or disconnected).<br>
 To avoid checking that, use `defer-to-connect`. It'll do that for you.
 
 ## Usage
@@ -19,9 +19,19 @@ deferToConnect(socket, () => {
 
 ## API
 
-### deferToConnect(socket, fn)
+### deferToConnect(socket, connectListener)
 
-Calls `fn()` when connected.
+Calls `connectListener()` when connected.
+
+### deferToConnect(socket, listeners)
+
+#### listeners
+
+An object representing `connect`, `secureConnect` and `close` properties.
+
+Calls `connect()` when the socket is connected.<br>
+Calls `secureConnect()` when the socket is securely connected.<br>
+Calls `close()` when the socket is destroyed.
 
 ## License
 
--- test.ts
+++ test.ts
@@ -1,38 +1,139 @@
-import * as https from 'https';
-import {Socket} from 'net';
+import {connect} from 'net';
+import {connect as secureConnect} from 'tls';
 import test from 'ava';
 import pEvent from 'p-event';
 import deferToConnect from './source';
 
-import sinon = require('sinon');
+const delay = async (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms));
 
-test('calls the callback if already connected', async t => {
-	const spy = sinon.spy();
-	const request = https.get('https://httpbin.org/anything');
+test('connect listener - socket has been already connected', async t => {
+	let called = false;
+	const socket = connect(53, '8.8.8.8');
 
-	const socket: Socket = await pEvent(request, 'socket');
 	await pEvent(socket, 'connect');
-	deferToConnect(socket, spy);
-
-	t.true(spy.calledOnce);
-});
-
-test('waiting till the connect event to call the callback', async t => {
-	const spy = sinon.spy();
-	const request = https.get('https://httpbin.org/anything');
-
-	const socket: Socket = await pEvent(request, 'socket');
-
-	deferToConnect(socket, spy);
-	t.true(spy.notCalled);
-
-	const promise = new Promise(resolve => {
-		socket.once('connect', () => {
-			t.true(spy.calledOnce);
-
-			resolve();
-		});
+	deferToConnect(socket, () => {
+		called = true;
 	});
 
-	await promise;
+	t.true(called);
+});
+
+test('connect listener - socket hasn\'t been connected yet', async t => {
+	let called = false;
+	const socket = connect(53, '8.8.8.8');
+
+	deferToConnect(socket, () => {
+		called = true;
+	});
+
+	await pEvent(socket, 'connect');
+
+	t.true(called);
+});
+
+test('connect listener - not executed, socket has been disconnected', async t => {
+	let called = false;
+
+	const socket = connect(53, '8.8.8.8');
+	await pEvent(socket, 'connect');
+	socket.end();
+
+	/* istanbul ignore next: this is on purpose */
+	deferToConnect(socket, () => {
+		called = true;
+	});
+
+	t.false(called);
+});
+
+test('connect listener - as a Listener property', async t => {
+	let called = false;
+	const socket = connect(53, '8.8.8.8');
+
+	await pEvent(socket, 'connect');
+	deferToConnect(socket, {
+		connect: () => {
+			called = true;
+		}
+	});
+
+	t.true(called);
+});
+
+test('close listener - socket has been already connected', async t => {
+	let called = false;
+	const socket = connect(53, '8.8.8.8');
+
+	await pEvent(socket, 'connect');
+	deferToConnect(socket, {
+		close: () => {
+			called = true;
+		}
+	});
+
+	socket.destroy();
+
+	await delay(1);
+
+	t.true(called);
+});
+
+test('close listener - socket has been already closed', async t => {
+	let called = false;
+	const socket = connect(53, '8.8.8.8');
+
+	await pEvent(socket, 'connect');
+	socket.destroy();
+
+	deferToConnect(socket, {
+		close: () => {
+			called = true;
+		}
+	});
+
+	t.true(called);
+});
+
+test('secureConnect listener - socket has been already securely connected', async t => {
+	let called = false;
+	const socket = secureConnect(443, 'example.com');
+
+	await pEvent(socket, 'secureConnect');
+	deferToConnect(socket, {
+		secureConnect: () => {
+			called = true;
+		}
+	});
+
+	t.true(called);
+});
+
+test('secureConnect listener - socket hasn\'t been securely connected yet', async t => {
+	let called = false;
+	const socket = secureConnect(443, 'example.com');
+
+	deferToConnect(socket, {
+		secureConnect: () => {
+			called = true;
+		}
+	});
+
+	await pEvent(socket, 'secureConnect');
+
+	t.true(called);
+});
+
+test('secureConnect listener - not executed, socket has been disconnected', async t => {
+	let called = false;
+
+	const socket = secureConnect(443, 'example.com');
+	await pEvent(socket, 'secureConnect');
+	socket.end();
+
+	/* istanbul ignore next: this is on purpose */
+	deferToConnect(socket, () => {
+		called = true;
+	});
+
+	t.false(called);
 });
